d5db557390b0c0868cd63c5fd771197e7569be0d
chenhao14762 <17335881197@163.com>
2020-12-10 18:06:55 +0800

    首页/注册登录页/列表页

--- gulp_test/src/js/list.js
+++ gulp_test/src/js/list.js
@@ -114,6 +114,120 @@ class list{
             location.href = 'list.min.html'
         })
 
+
+        //商城/拍卖
+        $(document).ready(function(){
+            $('.pick').css({
+                color : function(){
+                    return '#e93b39';
+                },
+                borderTop : function(){
+                    return '2px solid #e93b39';
+                }
+            })
+            $('.buy').click(function(){
+                $('.buy').css({
+                    color : function(){
+                        return '#e93b39';
+                    },
+                    borderTop : function(){
+                        return '2px solid #e93b39';
+                    }
+                })
+                $('.pick').removeAttr('style');
+            })
+            $('.pick').click(function(){
+                $('.pick').css({
+                    color : function(){
+                        return '#e93b39';
+                    },
+                    borderTop : function(){
+                        return '2px solid #e93b39';
+                    }
+                })
+                $('.buy').removeAttr('style');
+            })
+        })
+
+        //控制按钮点击
+        $(document).ready(function(){
+            $('.pro_control_btn a').eq(0).addClass('redbg')
+        })
+        $('.pro_control_btn a').each(function(i,value){
+            $(value).click(function(){
+                $(this).toggleClass('redbg')
+                $(this).siblings().removeClass('redbg')
+            })
+        })
+
+        //doller
+        $('.two_inp span input').focus(function(){
+            $('.two_inp span input').css({
+                width : function(){
+                    return '45px';
+                },
+                height : function(){
+                    return '22px'
+                }
+            })
+        })
+        $('.two_inp span input').blur(function(){
+            $('.two_inp span input').css({
+                width : function(){
+                    return '33px';
+                },
+                height : function(){
+                    return '14px'
+                }
+            })
+        })
+
+
+        //选择框
+        $('.pro_control_check label').each(function(i,value){
+            $(value).click(function(){
+                $(this).toggleClass("selectt")
+            })
+        })
+
+
+        //搜索框
+        $('.search').focus(function(){
+            $('.search').attr('value','');
+        })
+        
+        $('.search').blur(function(){
+            if($('.search').val() == ''){
+                $('.search').attr('value','在结果中搜索');
+            }else{
+                $('.search').val();
+            }
+        })
+
+
+        //切换卡粘性定位动态阴影
+        
+        $(window).scroll(function(){
+            if($('.price').position().top <= 60){
+                $('.price').removeAttr('id');
+            }
+            if($('.price').position().top > 60){
+                $('.price').attr('id','skity');
+            }
+            // console.log(parseInt($('.price').position().top));
+        })
+        $(window).ready(function(){
+            if($('.price').position().top <= 60){
+                $('.price').removeAttr('id');
+            }
+            if($('.price').position().top > 60){
+                $('.price').attr('id','skity');
+            } 
+            //页面刷新时在列表部分
+            $("html, body").scrollTop(0).animate({scrollTop: $(".pro_top").offset().top});
+        })
+            
+
     }
 }
 new list();
